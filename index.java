c85309073f45ce3936e7d9fbab9819ea05051fa8
Dusk99 <Diddaboy04@gmail.com>
2020-10-21 00:49:07 +0000

    done.

--- index.java
+++ index.java
@@ -1,35 +1,36 @@
-int rectpos = 150;
-int cirpos = 250;
+background(255);
 
+int rectx = 250;
+int recty = 250;
+int rectl = 100;
+int rectw = 100;
 
-
-
-
+int b = random(0,255);
+int g = random(0,255);
+int r = random(0,255);
 void setup() {
+
 	size(500, 500);
 }
 
 void draw() {
-// Step 1: Write code that draws the following screenshots 
-// with hard-coded values. (Feel free to use colors 
-// instead of grayscale.)
+stroke(0);
+fill (r,g,b,);
+rect(rectx,recty,rectw,rectl);
+fill((int) random(0,255), (int) random(0,255), (int) random(0,255))
+ellipse(mouseX + (int) random(-30,30), mouseY + (int) random(-30,30),10,10)
 
-// Step 2: Replace all of the hard-coded numbers with 
-// variables.
+void mousePressed(){
+ b = (int) random(0,255);
+ g = (int) random(0,255);
+ r = (int) random(0,255);
+ background(255);
+ rectx = random(0,400);
+ recty = random(0,400);
+ rectl = random(50,400);
+ rectw = random(50,400);
 
-// Step 3: Write assignment operations in draw() that 
-// change the value of the variables.
-// For example, “variable1 = variable1 + 2;”. 
+}
 
-// Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
-
-  background(255);
-  stroke(0);
-  fill(170)
-  rect (rectpos,rectpos,200,200)
-  fill(255,80,125)
-  ellipse (cirpos,cirpos,100,100,)
-  cirpos+=1
-  
 
 }
